6dbfe4515957130d42b0938363bf7669c9b2ba1f
Gajendra Deshpande <gcdeshpande@hotmail.com>
2020-12-18 11:27:27 +0530

    Add files via upload

---
+++ iNLTK/SetLanguage.py
@@ -0,0 +1,53 @@
+from inltk.inltk import setup
+#Native Languages
+#Hindi
+setup('hi')
+
+#Panjabi
+setup('pa')
+
+#Sanskrit
+setup('sa')
+
+#Gujarati
+setup('gu')
+
+#Kannada
+setup('kn')
+
+#Malayalam
+setup('ml')
+
+#Nepali
+setup('ne')
+
+#Oriya
+setup('or')
+
+#Marathi
+setup('mr')
+
+#Bengali
+setup('bn')
+
+#Tamil
+setup('ta')
+
+#Urdu
+setup('ur')
+
+#Telugu
+setup('te')
+
+#English
+setup('en')
+
+#Code-mixed languages
+#Hinglish (Hindi and English)
+setup('hi-en')
+
+#Tanglish (Tamil and English)
+setup('ta-en')
+
+#Manglish (Malayalam and English)
+setup('ml-en')
