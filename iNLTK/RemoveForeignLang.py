02b42607588101fbce38a7e323939f2ccb3f4423
Gajendra Deshpande <gcdeshpande@hotmail.com>
2020-12-19 11:24:55 +0530

    Create RemoveForeignLang.py

---
+++ iNLTK/RemoveForeignLang.py
@@ -0,0 +1,2 @@
+from inltk.inltk import remove_foreign_languages
+remove_foreign_languages('विकिपीडिया सभी विषयों ਇੱਕ ਅਲੌਕਿਕ ਨਜ਼ਾਰਾ ਬੱਝਾ ਹੋਇਆ ਸਾਹਮਣੇ ਆ ਖਲੋਂਦਾ ਸੀ पर प्रामाणिक और 维基百科:关于中文维基百科 उपयोग, परिवर्तन 维基百科:关于中文维基百科', 'hi')
